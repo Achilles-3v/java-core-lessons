ae8ba639d055b4609c96a6a9566ab3e92c56aab4
Achilles-3v <sh17vv@gmail.com>
2023-12-03 19:59:57 +0500

    BuilderAndBuffer class modified

--- src/chapter3/stringtasks/builderandbuffer/BuilderAndBuffer.java
+++ src/chapter3/stringtasks/builderandbuffer/BuilderAndBuffer.java
@@ -3,11 +3,11 @@ package chapter3.stringtasks.builderandbuffer;
 public class BuilderAndBuffer {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
-        String value = "";
+        StringBuilder value = new StringBuilder();
         for(int i = 0; i < 100_000; i++) {
-            value += i;
+            value.append(i);
         }
         long endTime = System.currentTimeMillis();
-        System.out.println("Time: " + (startTime - endTime));
+        System.out.println("Time: " + (endTime - startTime));
     }
 }
