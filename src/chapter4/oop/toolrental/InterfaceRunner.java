7169f161dc14c07310de4ae6b1ced68a6eba6f0e
Achilles-3v <sh17vv@gmail.com>
2023-12-04 20:47:31 +0500

    interface: default and private method added

--- src/chapter4/oop/toolrental/InterfaceRunner.java
+++ src/chapter4/oop/toolrental/InterfaceRunner.java
@@ -5,12 +5,18 @@ public class InterfaceRunner {
         Printable drill = new Drill(new Price(11000), new Brand("DeWalt"), 5000);
         Printable weldingMachine = new WeldingMachine(new Price(15300), new Brand("Interskol"));
 
-        print(drill, weldingMachine);
+        printWithRandom(drill, weldingMachine);
     }
 
     public static void print(Printable... objects) {
         for (Printable object : objects) {
             object.print();
+        }
+    }
+
+    public static void printWithRandom(Printable... objects) {
+        for (Printable object : objects) {
+            object.printWithRandom();
         }
     }
 }
--- src/chapter4/oop/toolrental/Printable.java
+++ src/chapter4/oop/toolrental/Printable.java
@@ -1,7 +1,19 @@
 package chapter4.oop.toolrental;
 
+import java.util.Random;
+
 public interface Printable {
     String NAME = "Name";
+    Random RANDOM = new Random();
+
+    default void printWithRandom() {
+        System.out.println(generateRandom());
+        print();
+    }
 
     void print();
+
+    private int generateRandom() {
+        return RANDOM.nextInt();
+    }
 }
