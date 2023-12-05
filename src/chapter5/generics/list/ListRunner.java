807e62a076e540180f0e279d22e23f6f09ce12d1
Achilles-3v <sh17vv@gmail.com>
2023-12-06 01:15:40 +0500

    List class added

---
+++ src/chapter5/generics/list/ListRunner.java
@@ -0,0 +1,13 @@
+package chapter5.generics.list;
+
+public class ListRunner {
+
+    public static void main(String[] args) {
+        List list = new List(10);
+        list.add("String1");
+        list.add("String2");
+
+        String element = (String) list.get(1);
+        System.out.println(element);
+    }
+}
