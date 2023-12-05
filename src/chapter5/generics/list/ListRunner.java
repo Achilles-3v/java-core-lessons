734b8ef9910fc03a40c493444a4a9855390a631f
Achilles-3v <sh17vv@gmail.com>
2023-12-06 01:26:12 +0500

    generics added

--- src/chapter5/generics/list/ListRunner.java
+++ src/chapter5/generics/list/ListRunner.java
@@ -3,11 +3,11 @@ package chapter5.generics.list;
 public class ListRunner {
 
     public static void main(String[] args) {
-        List list = new List(10);
+        List<String> list = new List<>(10);
         list.add("String1");
         list.add("String2");
 
-        String element = (String) list.get(1);
+        String element = list.get(1);
         System.out.println(element);
     }
 }
