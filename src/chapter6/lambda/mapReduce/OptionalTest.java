7551694d6c1abc072427a471a1cdf6ac4d9ec5d7
Achilles-3v <sh17vv@gmail.com>
2023-12-14 23:21:17 +0500

    chapter6.lambda.mapReduce OptionalTest

--- src/chapter6/lambda/mapReduce/OptionalTest.java
+++ src/chapter6/lambda/mapReduce/OptionalTest.java
@@ -1,17 +1,18 @@
 package chapter6.lambda.mapReduce;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
  * В этой программе демонстрируется
- * использование Stream и ссылки на метод
+ * использование Optional и ссылки на метод
  * @version 1.0 2023-12-14
  */
-public class MapReduceTest {
+public class OptionalTest {
 
     public static void main(String[] args) {
 
-        Stream.of(
+        Optional<Student> maybeStudent = Stream.of(
                 new Student(10, "Oleg"),
                 new Student(102, "Igor"),
                 new Student(33, "Mary"),
@@ -23,10 +24,11 @@ public class MapReduceTest {
                 new Student(14, "Bob"),
                 new Student(74, "Sam")
         )
-                .parallel()
-                .map(Student::getAge)
-                .reduce(Math::max)
-                .ifPresent(System.out::println);
+                .sequential()
+                .reduce((student, student2) -> student.getAge() > student2.getAge() ? student : student2);
 
+        maybeStudent.map(Student::getAge)
+                .flatMap(age -> Optional.of(age * 2))
+                .ifPresent(System.out::println);
     }
 }
