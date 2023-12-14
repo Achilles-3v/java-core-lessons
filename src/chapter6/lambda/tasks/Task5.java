83955e92faf27be73777a9f860d1da969dd0511c
Achilles-3v <sh17vv@gmail.com>
2023-12-15 03:53:30 +0500

    chapter6.lambda Person

---
+++ src/chapter6/lambda/tasks/Task5.java
@@ -0,0 +1,15 @@
+package chapter6.lambda.tasks;
+
+/**
+ * Дан класс Person с полями firstName, lastName и
+ * age. Вывести имя самого старшего человека, у
+ * которого длина этого имени не привышает 15 символов
+ * @version 1.5 2023-12-15
+ */
+public class Task5 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
