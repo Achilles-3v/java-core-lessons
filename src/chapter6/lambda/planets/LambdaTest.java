33625df941337edc262d4e1bd1194bfee677b7d0
Achilles-3v <sh17vv@gmail.com>
2023-12-14 16:09:13 +0500

    chapter6.lambda.planets LambdaTest

---
+++ src/chapter6/lambda/planets/LambdaTest.java
@@ -0,0 +1,35 @@
+package chapter6.lambda.planets;
+
+import java.util.*;
+
+import javax.swing.*;
+import javax.swing.Timer;
+
+/**
+ * В этой программе демонстрируется
+ * применение лямбда-выражений
+ * @version 1.0 2023-12-14
+ * @author Cay Horstmann
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+
+        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
+        "Jupiter", "Saturn", "Uranus", "Neptune" };
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted in dictionary order:");
+        Arrays.sort(planets);
+        System.out.println(Arrays.toString(planets));
+        System.out.println("Sorted by length:");
+        Arrays.sort(planets, (first, second) -> first.length() - second.length());
+        System.out.println(Arrays.toString(planets));
+
+        Timer t = new Timer(1000, event ->
+                System.out.println("The time is " + new Date()));
+        t.start();
+
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
+    }
+}
