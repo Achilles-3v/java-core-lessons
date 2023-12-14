e893652693cfd08a1155e22a15a84f4d4d108aef
Achilles-3v <sh17vv@gmail.com>
2023-12-14 19:57:21 +0500

    chapter6.lambda.innerClass InnerClassTest

--- src/chapter6/lambda/innerClass/InnerClassTest.java
+++ src/chapter6/lambda/innerClass/InnerClassTest.java
@@ -16,7 +16,11 @@ public class InnerClassTest {
 
     public static void main(String[] args) {
 
+        var clock = new TalkingClock(1000, true);
+        clock.start();
 
+        JOptionPane.showMessageDialog(null, "Quit program?");
+        System.exit(0);
     }
 }
 
