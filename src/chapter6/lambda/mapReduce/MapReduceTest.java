6c87e6ed182bddcdfd12403f51369e0572556e19
Achilles-3v <sh17vv@gmail.com>
2023-12-14 21:53:10 +0500

    chapter6.lambda.mapReduce MapReduceTest

--- src/chapter6/lambda/mapReduce/MapReduceTest.java
+++ src/chapter6/lambda/mapReduce/MapReduceTest.java
@@ -1,4 +1,26 @@
 package chapter6.lambda.mapReduce;
 
+import java.util.stream.Stream;
+
 public class MapReduceTest {
+
+    public static void main(String[] args) {
+
+        Stream.of(
+                new Student(10, "Oleg"),
+                new Student(102, "Igor"),
+                new Student(33, "Mary"),
+                new Student(54, "Carl"),
+                new Student(78, "Hack"),
+                new Student(72, "Vera"),
+                new Student(32, "Rose"),
+                new Student(41, "Stanislav"),
+                new Student(14, "Bob"),
+                new Student(74, "Sam")
+        )
+                .map(Student::getAge)
+                .reduce(Math::max)
+                .ifPresent(System.out::println);
+
+    }
 }
